a3918779d396f8fce3a3cbd88452114b8917322a
Grib <alex.dendisol@gmail.com>
2025-04-29 12:37:11 +0300

    frontend Add GetHotelsRequest и GetHotelRequest

---
+++ Application/DTO/Hotel/ClientRequest/GetHotelRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.DTO.Hotel.ClientRequest
+{
+  public record GetHotelRequest(Guid hotelId) : IRequest<GetHotelRequest.Response>
+  {
+    public const string RouteTemplate = "/api/hotels/{hotelId}";
+
+    public record Response(HotelDto Hotel);
+  }
+}
