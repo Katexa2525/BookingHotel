5ca9c2bfc081b58b8dc4e54fc4cb42e309cfbd77
Grib <alex.dendisol@gmail.com>
2025-05-01 12:51:41 +0300

    frontend Add UploadHotelImageHandler

---
+++ BookingHotel/Features/ManageHotel/Mediatr/UploadHotelImageHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.DTO.Hotel.ClientRequest;
+using MediatR;
+
+namespace BookingHotel.Features.ManageHotel.Mediatr
+{
+  public class UploadHotelImageHandler : IRequestHandler<UploadHotelImageRequest, UploadHotelImageRequest.Response>
+  {
+    private readonly HttpClient _httpClient;
+
+    public UploadHotelImageHandler(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+
+    public async Task<UploadHotelImageRequest.Response> Handle(UploadHotelImageRequest request, CancellationToken cancellationToken)
+    {
+      var fileContent = request.File.OpenReadStream(request.File.Size, cancellationToken);
+
+        using var content = new MultipartFormDataContent();
+        content.Add(new StreamContent(fileContent), "image", request.File.Name);
+
+        var response = await _httpClient.PostAsync(UploadHotelImageRequest.RouteTemplate.Replace("{hotelId}", request.HotelId.ToString()), content, cancellationToken);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var fileName = await response.Content.ReadAsStringAsync(cancellationToken: cancellationToken);
+            return new UploadHotelImageRequest.Response(fileName);
+        }
+        else
+        {
+            return new UploadHotelImageRequest.Response("");
+        }
+    }
+  }
+}
