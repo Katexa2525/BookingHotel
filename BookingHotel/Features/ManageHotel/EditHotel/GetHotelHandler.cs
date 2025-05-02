bc5371fa3087e46d521fffdf6f881eacb07d09ba
A.Denisov <A.Denisov2@beloil.by>
2025-05-02 16:53:13 +0300

    frontend Добавлены EditHotelHandler и GetHotelHandler

---
+++ BookingHotel/Features/ManageHotel/EditHotel/GetHotelHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTO.Hotel.ClientRequest;
+using MediatR;
+using System.Net.Http.Json;
+
+namespace BookingHotel.Features.ManageHotel.EditHotel
+{
+  public class GetHotelHandler : IRequestHandler<GetHotelRequest, GetHotelRequest.Response?>
+  {
+    private readonly HttpClient _httpClient;
+
+    public GetHotelHandler(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+
+    public async Task<GetHotelRequest.Response?> Handle(GetHotelRequest request, CancellationToken cancellationToken)
+    {
+      try
+      {
+        //Заполнитель hotelId в RouteTemplate заменяется идентификатором отеля, подлежащему редактированию, перед выполнением HTTP-запроса
+        return await _httpClient.GetFromJsonAsync<GetHotelRequest.Response>(GetHotelRequest.RouteTemplate.Replace("{hotelId}", 
+                                                                            request.hotelId.ToString()));
+      }
+      catch (HttpRequestException)
+      {
+        return default!;
+      }
+    }
+  }
+}
