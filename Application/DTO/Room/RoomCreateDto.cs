159b3d80715f543f10f7c12ed09f27c9640e5580
Grib <alex.dendisol@gmail.com>
2025-05-05 02:40:54 +0300

    frontend Добавлено поле Description в модель Room, создана миграция, добавлено поле на форму

--- Application/DTO/Room/RoomCreateDto.cs
+++ Application/DTO/Room/RoomCreateDto.cs
@@ -8,6 +8,8 @@ namespace Application.DTO.Room
   {
     public int PeopleNumber { get; set; }
     public double Square { get; set; }
+    /// <summary> Описание номера </summary>
+    public string Description { get; set; } = string.Empty;
     public Guid RoomTypeId { get; set; }
     public IEnumerable<PriceCreateDto> Prices { get; set; }
     public IEnumerable<RoomPhotoCreateDto> RoomPhotos { get; set; }
