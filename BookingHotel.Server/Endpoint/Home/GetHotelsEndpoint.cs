5eb01643086d7e18d2251e689e3f740d887f2645
Grib <alex.dendisol@gmail.com>
2025-05-04 17:18:39 +0300

    frontend Add GetHotelsHandler and GetHotelsEndpoint Edit HomePage

---
+++ BookingHotel.Server/Endpoint/Home/GetHotelsEndpoint.cs
@@ -0,0 +1,41 @@
+﻿using Application.ConstMessages;
+using Application.DTO.Hotel.ClientRequest;
+using Application.DTO.Hotel.CQRS;
+using Ardalis.ApiEndpoints;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHotel.Server.Endpoint.Home
+{
+  public class GetHotelsEndpoint : BaseAsyncEndpoint.WithRequest<Guid>.WithResponse<GetHotelsRequest.Response>
+  {
+    private readonly IMediator _mediator;
+
+    public GetHotelsEndpoint(IMediator mediator)
+    {
+      _mediator = mediator;
+    }
+
+    [HttpGet(GetHotelsRequest.RouteTemplate)]
+    public override async Task<ActionResult<GetHotelsRequest.Response>> HandleAsync(Guid request, CancellationToken cancellationToken = default)
+    {
+      // получаю все отели из БД
+      var hotels = await _mediator.Send(new GetAllHotelQuery());
+      if (hotels is null)
+      {
+        return BadRequest(AppMessage.GetHotelAllTextErrorMessage);
+      }
+      //Guid Id, string Name, string Description, string Location, double Rating, int Star, string? MainPhoto
+
+      // Если отель найден, возвращается новый экземпляр GetHotelRequest.Response, содержащий сведения о нём
+      var response = new GetHotelsRequest.Response(
+        hotels.Select(hotel => new GetHotelsRequest.HotelAllDto( 
+          hotel.Id, hotel.Name, hotel.Description, hotel.Location, hotel.Rating, hotel.Star, hotel.MainPhoto
+          ))
+        );
+
+      return Ok(response);
+
+    }
+  }
+}
---
+++ BookingHotel/Features/ManageHotel/Shared/GetHotelsHandler.cs
@@ -0,0 +1,30 @@
+﻿using Application.DTO.Hotel.ClientRequest;
+using MediatR;
+using System.Net.Http.Json;
+
+namespace BookingHotel.Features.ManageHotel.Shared
+{
+  public class GetHotelsHandler : IRequestHandler<GetHotelsRequest, GetHotelsRequest.Response?>
+  {
+    private readonly HttpClient _httpClient;
+
+    public GetHotelsHandler(HttpClient httpClient)
+    {
+      _httpClient = httpClient;
+    }
+
+    public async Task<GetHotelsRequest.Response?> Handle(GetHotelsRequest request, CancellationToken cancellationToken)
+    {
+      try
+      {
+        // Выполняется запрос к API. В случае успеха ответ десериализуется и возвращается вызывающей стороне
+        return await _httpClient.GetFromJsonAsync<GetHotelsRequest.Response>(GetHotelsRequest.RouteTemplate);
+      }
+      catch (HttpRequestException) 
+      {
+        //В противном случае вызывающая сторона получает в качестве ответа null
+        return default!;
+      }
+    }
+  }
+}
