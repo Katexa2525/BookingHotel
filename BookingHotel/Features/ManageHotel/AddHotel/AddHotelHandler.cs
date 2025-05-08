7967789621988a900f45604e254896b573a26cb6
A.Denisov <A.Denisov2@beloil.by>
2025-05-08 15:50:14 +0300

    frontend Мелкие правки

--- BookingHotel/Features/ManageHotel/AddHotel/AddHotelHandler.cs
+++ BookingHotel/Features/ManageHotel/AddHotel/AddHotelHandler.cs
@@ -20,9 +20,6 @@ namespace BookingHotel.Features.ManageHotel.Mediatr
     /// <returns></returns> 
     public async Task<AddHotelRequest.Response> Handle(AddHotelRequest request, CancellationToken cancellationToken)
     {
-      // HttpClient используется для вызова API с использованием шаблона маршрута, который определили для запроса
-      //var response = await _httpClient.PostAsJsonAsync(AddHotelRequest.RouteTemplate, request, cancellationToken);
-
       // Защищенный HttpClient используется для вызова API с использованием шаблона маршрута, который определили для запроса
       HttpClient? httpClient = _httpClientFactory.CreateClient("SecureAPIClient");
       httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
@@ -30,7 +27,6 @@ namespace BookingHotel.Features.ManageHotel.Mediatr
 
       if (response.IsSuccessStatusCode)
       {
-        //int hotelId = await response.Content.ReadFromJsonAsync<int>(cancellationToken);
         Guid hotelId = await response.Content.ReadFromJsonAsync<Guid>(cancellationToken);
         // если запрос был успешным, то hotelId считывается из ответа и возвращается с помощью записи AddHotelRequest.Response
         return new AddHotelRequest.Response(hotelId);
