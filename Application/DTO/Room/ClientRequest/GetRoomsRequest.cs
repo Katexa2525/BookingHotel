a80751f6d83a06a6356df340a001e0d6d176c543
Grib <alex.dendisol@gmail.com>
2025-05-12 01:54:42 +0300

    fronttend Добавлена модель для получения данных по номерам

---
+++ Application/DTO/Room/ClientRequest/GetRoomsRequest.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTO.Hotel.ClientRequest;
+using MediatR;
+
+namespace Application.DTO.Room.ClientRequest
+{
+  public record GetRoomsRequest : IRequest<GetRoomsRequest.Response>
+  {
+    public const string RouteTemplate = "api/rooms";
+
+    public record Response(List<RoomAllDto> Rooms);
+  }
+}
