5e42ad33be6c488840ceae464e35978e72eb377e
Grib <alex.dendisol@gmail.com>
2025-05-13 00:21:45 +0300

    frontend Add RoomFacilityHasData migration

---
+++ Infrastructure/Migrations/20250512211911_RoomFacilityHasData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RoomFacilityHasData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                columns: new[] { "Id", "Name", "RoomId" },
+                values: new object[,]
+                {
+                    { new Guid("13b17dab-7ed0-40d9-93d5-b020eea0aad9"), "Ванна", new Guid("e12d0ba1-7ac8-4a19-b241-31705c2a3500") },
+                    { new Guid("427ba46a-07e6-4da2-afed-c1760f02a636"), "Фен", new Guid("e12d0ba1-7ac8-4a19-b241-31705c2a3500") },
+                    { new Guid("4c17ade5-2349-43fe-a9d1-d10aed6ddebd"), "Цифровое ТВ", new Guid("6b09f078-169f-4ae8-aaa1-5b8a2e1c896b") },
+                    { new Guid("573716d8-e318-4ba0-957c-2a1a0be4af30"), "Wi-Fi", new Guid("e12d0ba1-7ac8-4a19-b241-31705c2a3500") },
+                    { new Guid("6e69e113-eb98-4094-b417-83d61a9ab4eb"), "Мини-холодильник", new Guid("6b09f078-169f-4ae8-aaa1-5b8a2e1c896b") },
+                    { new Guid("a774e890-3726-4e5b-8071-50e01f27b7b9"), "Цифровое ТВ", new Guid("e12d0ba1-7ac8-4a19-b241-31705c2a3500") },
+                    { new Guid("d3fb6a18-f736-4cb5-bf64-69c36b3d489c"), "Wi-Fi", new Guid("6b09f078-169f-4ae8-aaa1-5b8a2e1c896b") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                keyColumn: "Id",
+                keyValue: new Guid("13b17dab-7ed0-40d9-93d5-b020eea0aad9"));
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                keyColumn: "Id",
+                keyValue: new Guid("427ba46a-07e6-4da2-afed-c1760f02a636"));
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                keyColumn: "Id",
+                keyValue: new Guid("4c17ade5-2349-43fe-a9d1-d10aed6ddebd"));
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                keyColumn: "Id",
+                keyValue: new Guid("573716d8-e318-4ba0-957c-2a1a0be4af30"));
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                keyColumn: "Id",
+                keyValue: new Guid("6e69e113-eb98-4094-b417-83d61a9ab4eb"));
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                keyColumn: "Id",
+                keyValue: new Guid("a774e890-3726-4e5b-8071-50e01f27b7b9"));
+
+            migrationBuilder.DeleteData(
+                schema: "dbo",
+                table: "RoomFacilities",
+                keyColumn: "Id",
+                keyValue: new Guid("d3fb6a18-f736-4cb5-bf64-69c36b3d489c"));
+        }
+    }
+}
