7392ea11e9f44991dfe86a4fb9eaafbd79f7f1f6
Grib <alex.dendisol@gmail.com>
2025-05-18 13:03:18 +0300

    frontend Add Migration RoomEditModelMigration

--- Domain/Models/Room.cs
+++ Domain/Models/Room.cs
@@ -7,13 +7,15 @@
     public Hotel Hotel { get; set; }
     public int PeopleNumber { get; set; }
     public double Square { get; set; }
+    /// <summary> Основное фото отеля </summary>
+    public string? MainPhoto { get; set; } = string.Empty;
     /// <summary> Описание номера </summary>
-    public string Description { get; set; } = string.Empty;
-    public ICollection<Price> Prices { get; set; }
-    public ICollection<RoomPhoto> RoomPhotos { get; set; }
-    public ICollection<RoomFacility> RoomFacilities { get; set; }
+    public string? Description { get; set; } = string.Empty;
+    public ICollection<Price>? Prices { get; set; }
+    public ICollection<RoomPhoto>? RoomPhotos { get; set; }
+    public ICollection<RoomFacility>? RoomFacilities { get; set; }
     public Guid RoomTypeId { get; set; }
     public RoomType RoomType { get; set; }
-    public ICollection<Booking> Bookings { get; set; }
+    public ICollection<Booking>? Bookings { get; set; }
   }
 }
