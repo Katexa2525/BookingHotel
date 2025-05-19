46993bfb3e5a602a97e4da8684cb0fc060378e44
Grib <alex.dendisol@gmail.com>
2025-05-19 03:46:12 +0300

    frontend Add Migration GuestBookingEditModelMigration

--- Application/DTO/Booking/BookingCreateDto.cs
+++ Application/DTO/Booking/BookingCreateDto.cs
@@ -7,12 +7,13 @@ namespace Application.DTO.Booking
   {
     public string Name { get; set; } = string.Empty;
     public int NumberOfAdults { get; set; }
+    public int NumberOfChilds { get; set; }
     /// <summary> Дата начала  </summary>
     public DateTime ArrivalDate { get; set; }
     /// <summary> Дата окончания </summary>
     public DateTime DepartureDate { get; set; }
     /// <summary> Комментарий к бронированию </summary>
-    public string Description { get; set; } = string.Empty;
+    public string? Description { get; set; } = string.Empty;
     public IEnumerable<GuestDto> Guests { get; set; }
     public IEnumerable<ServiceDto> Services { get; set; }
   }
