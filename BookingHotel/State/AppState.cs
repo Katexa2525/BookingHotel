f8df8b76aec4fa0dd0ab9de6cbc5102e57410ae9
Grib <alex.dendisol@gmail.com>
2025-05-25 10:43:12 +0300

    frontend Добавлена функциональность сохранения состояния

---
+++ BookingHotel/State/AppState.cs
@@ -0,0 +1,29 @@
+﻿using Blazored.LocalStorage;
+
+namespace BookingHotel.State
+{
+  public class AppState
+  {
+    private bool _isInitialized;
+
+    public FavoriteHotelsState FavoriteHotelsState { get; }
+
+    public AppState(ILocalStorageService localStorageService)
+    {
+      //FavoriteHotelsState передаю в ILocalStorageService
+      FavoriteHotelsState = new FavoriteHotelsState(localStorageService);
+    }
+
+    /// <summary>Метод Initialize предоставляет основное место для инициализации любых дочерних хранилищ состояний</summary>
+    public async Task Initialize()
+    {
+      if (!_isInitialized)
+      {
+        // Инициализация хранилища
+        await FavoriteHotelsState.Initialize();
+        _isInitialized = true;
+      }
+    }
+
+  }
+}
