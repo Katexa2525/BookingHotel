eeb93bb31b87f02f343580deff9e5d57f0a4699e
A.Denisov <A.Denisov2@beloil.by>
2025-05-27 16:37:41 +0300

    frontend Add handler review client

---
+++ BookingHotel/Features/ManageReview/AddReview/AddReviewHandler.cs
@@ -0,0 +1,49 @@
+﻿using Application.DTO.Review.ClientRequest;
+using MediatR;
+using System.Net.Http.Json;
+
+namespace BookingHotel.Features.ManageReview.AddReview
+{
+  public class AddReviewHandler : IRequestHandler<AddReviewRequest, AddReviewRequest.Response>
+  {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public AddReviewHandler(IHttpClientFactory httpClientFactory)
+    {
+      _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<AddReviewRequest.Response> Handle(AddReviewRequest request, CancellationToken cancellationToken)
+    {
+      try
+      {
+        // Защищенный HttpClient используется для вызова API с использованием шаблона маршрута, который определили для запроса
+        HttpClient? httpClient = _httpClientFactory.CreateClient("SecureAPIClient");
+        //httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+        var response = await httpClient.PostAsJsonAsync(AddReviewRequest.RouteTemplate, request, cancellationToken);
+
+        if (response.IsSuccessStatusCode)
+        {
+          Guid reviewId = await response.Content.ReadFromJsonAsync<Guid>(cancellationToken);
+          // если запрос был успешным, то roomId считывается из ответа и возвращается с помощью записи AddRoomRequest.Response
+          return new AddReviewRequest.Response(reviewId);
+        }
+        else
+        {
+          // если запрос не выполнен
+          return new AddReviewRequest.Response(Guid.Empty);
+        }
+      }
+      catch (HttpRequestException)
+      {
+        //В противном случае вызывающая сторона получает в качестве ответа null
+        return default!;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+        return default!;
+      }
+    }
+  }
+}
---
+++ BookingHotel/Features/ManageReview/EditReview/GetReviewHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.DTO.Review;
+using Application.DTO.Review.ClientRequest;
+using MediatR;
+using System.Net.Http.Json;
+
+namespace BookingHotel.Features.ManageReview.EditReview
+{
+  public class GetReviewHandler : IRequestHandler<GetReviewRequest, GetReviewRequest.Response?>
+  {
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public GetReviewHandler(IHttpClientFactory httpClientFactory)
+    {
+      _httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<GetReviewRequest.Response?> Handle(GetReviewRequest request, CancellationToken cancellationToken)
+    {
+      try
+      {
+        // Незащищенный HttpClient используется для вызова API с использованием шаблона маршрута, который определили для запроса
+        HttpClient? httpClient = _httpClientFactory.CreateClient("NoAuthenticationClient");
+
+        string route = GetReviewRequest.RouteTemplate.Replace("{reviewId}", request.reviewId.ToString());
+
+        var res = await httpClient.GetFromJsonAsync<ReviewDto?>(route, cancellationToken);
+
+        return new GetReviewRequest.Response(res);
+      }
+      catch (HttpRequestException ex)
+      {
+        Console.WriteLine(ex.Message);
+        return default!;
+      }
+    }
+  }
+}
