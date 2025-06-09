f96e37d9bc57cd439fd97fda3643440c743fe5e5
A.Denisov <A.Denisov2@beloil.by>
2025-06-09 10:11:11 +0300

    frontend Add ClientRequest classes

---
+++ Application/DTO/TypeFood/ClientRequest/GetTypeFoodRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.DTO.TypeFood.ClientRequest
+{
+  public record GetTypeFoodRequest(Guid typefoodId) : IRequest<GetTypeFoodRequest.Response>
+  {
+    public const string RouteTemplate = "/api/typefoods/{typefoodId}";
+
+    public record Response(TypeFoodDto? Food);
+  }
+}
