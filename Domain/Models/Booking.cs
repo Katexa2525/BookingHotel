640239cadd01d01657456137100a17bf1e8a64cb
A.Denisov <A.Denisov2@beloil.by>
2025-06-18 10:59:36 +0300

    frontend Adding field UserId to boolking model Have been Edit DTO models

--- Domain/Models/Booking.cs
+++ Domain/Models/Booking.cs
@@ -10,6 +10,8 @@
     public DateTime DepartureDate { get; set; }
     /// <summary> Комментарий к бронированию </summary>
     public string? Description { get; set; } = string.Empty;
+    /// <summary> id пользователя из Keycloak </summary>
+    public string? UserId { get; set; } = string.Empty;
     public ICollection<Guest> Guests { get; set; }
     public ICollection<Service> Services { get; set; }
   }
